10bb7451dfc3ff11d8b9bc65b2738c5e3415d2eb
Scott Reynolds <scottreynolds6294@gmail.com>
2024-09-08 20:52:42 -0400

    merge erros fixed

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,62 +1,42 @@
+
 const router = require('express').Router();
 const { User, Trip, Flight, Lodging, Itinerary } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
     if (req.session.logged_in) {
-        res.redirect('home');
+        res.redirect('dashboard');
         return;
     } res.render('login');
 });
 
 router.get("/", (req, res) => {
-    res.render("home");
+    res.render('dashboard');
   });
 
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
-        res.redirect('home');
+        res.redirect('dashboard');
         return;
     } res.render('login');
 })
 
 router.get('/signup', (req, res)=> {
-    if (req.sessiosn.logged_in) {
-        res.redirect('home');
+    if (req.session.logged_in) {
+        res.redirect('dashboard');
         return;
     }
     res.render('login')
 })
 
-router.get('/', async (req, res) => {
-    try {
-        const tripData = await Trip.findAll();
-        const trips = tripData.map((trip) => trip.get({ plain: true }))
-
-        res.render('home',  {
-            trips,
-            logged_in: req.session.logged_in,
-        });
-    } catch (err) {
-        res.status(500).json({ message: 'Failed to retrieve trips', error: err.message });
-    }
-});
-
-router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/');
-        return;
-    } res.render('login');
-});
-
-router.get('/home', withAuth, async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
     try {
         const tripData = await Trip.findAll({
             where: { user_id: req.session.user_id }, 
         });
 
         const trips = tripData.map((trip) => trip.get({ plain:true }));
-        res.render('home', {
+        res.render('dashboard', {
             trips,
             logged_in: req.session.logged_in,
         });
@@ -65,9 +45,8 @@ router.get('/home', withAuth, async (req, res) => {
     }
 });
 
-
 router.get('/trip', withAuth, (req, res) => {
-    res.render('trip', {
+    res.render('partials/start-trip', {
         logged_in: req.session.logged_in
     });
 });
@@ -90,12 +69,6 @@ router.get('/trip/:id', withAuth, async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-});
-
-router.get('/home/new', withAuth, (req, res) => {
-    res.render('trip', {
-        logged_in: req.session.logged_in,
-    });
 });
 
 module.exports = router;
