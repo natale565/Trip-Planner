5e070d50ea2308dc9143f14b4908b6d233ba28da
Scott Reynolds <scottreynolds6294@gmail.com>
2024-09-12 10:20:44 -0400

    updated styling for tripdetails page/made all cards the same size

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,17 +1,17 @@
 const formatDateTimeForDisplay = (dateString) => {
     const date = new Date(dateString);
     const options = {
-        weekday: 'short', // 'long' for full weekday name
-        month: 'short', // 'long' for full month name
+        weekday: 'short',
+        month: 'short',
         day: 'numeric',
         year: 'numeric',
         hour: '2-digit',
         minute: '2-digit',
-        hour12: true // Use 12-hour clock with AM/PM
+        hour12: true,
     };
     return date.toLocaleString('en-US', options);
 };
 
 
 
-module.exports = { formatDateTimeForDisplay };
+module.exports = { formatDateTimeForDisplay } ;
